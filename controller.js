b3c71b003fb207693824b0c934549a633fa69f5b
plx-aungkhant <plx.aungkhant@gmail.com>
2024-04-03 03:04:08 +0700

    modify: fix send/receive messages on all tabs                auto scrolldown messages are received

--- controller.js
+++ controller.js
@@ -1,9 +1,22 @@
-angular.module('webchat', []).
-    controller('ChatController', function ($scope, $http) {
+angular.module('webchat', [])
+    .directive('onFinishRender', function ($timeout) {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attr) {
+                if (scope.$last === true) {
+                    $timeout(function () {
+                        scope.$emit(attr.onFinishRender);
+                    });
+                }
+            }
+        }
+    })
+    .controller('ChatController', function ($scope, $http) {
         $scope.User = "";
         var isInitial = true;
         var page = 1;
         var pageSize = 25;
+        var skipscroll = false;
         loadStartup();
         $scope.sendMsg = function (_msg) {
             if (isInitial) {
@@ -16,21 +29,37 @@ angular.module('webchat', []).
 
             fetchData();
             $scope.SendText = "";
+            //scrollDown();
         }
+        $scope.$on('ngRepeatFinished', function (ngRepeatFinishedEvent) {
+            scrollDown();
+        });
 
+       
         //refresh message between tabs
         window.addEventListener('storage', function (event) {
             if (event.key === 'CHAT_DATA') {
                 fetchData();
+                $scope.$apply();
+                //scrollDown();
             }
         });
 
         $(".chat-thread").bind("scroll", function () {
             if ($(this).scrollTop() == 0) {
                 page++;
+                skipscroll = true;
                 fetchData();
             }
         });
+
+        function scrollDown() {
+            if (!skipscroll) {
+                var scroll = $('.chat-thread');
+                scroll.animate({ scrollTop: scroll[0].scrollHeight }, 500);
+            }
+            skipscroll = false;
+        }
 
         function loadStartup() {
             $scope._msgHistory = [{ USER_NAME: "Welcome!", MSG: "Please enter your User Name." }];
@@ -54,7 +83,7 @@ angular.module('webchat', []).
             storageData.forEach(function (e) {
                 e.IS_ME = e.USER_NAME == $scope.User ? true : false;
             });
-            $scope._msgHistory = storageData;
+            $scope._msgHistory = storageData;            
         }
 
         function setDatabyUser(Msg) {
@@ -64,10 +93,8 @@ angular.module('webchat', []).
             else {
                 storageData = [];
             }
-            
+
             storageData.push({ USER_NAME: $scope.User, MSG: Msg, DATE_TIME: new Date() });
             localStorage.CHAT_DATA = JSON.stringify(storageData);
         }
-    });  
-
-
+    });
